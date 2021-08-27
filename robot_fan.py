cfdea9138352d5a03ad4a3df62bad4bf40fc3fd5
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-08-27 23:00:00 +0300

    Implement the activation of the driving

--- robot_fan.py
+++ robot_fan.py
@@ -1,15 +1,29 @@
 import mindstorms
 
+ACTIVATION_COLOR = "red"
+
 MINIMAL_ANGLE = 275
 MAXIMAL_ANGLE = 220
 
 MINIMAL_SPEED = -10
 MAXIMAL_SPEED = -100
 
+control_color_sensor = mindstorms.ColorSensor("F")
 sensory_motor = mindstorms.Motor("D")
 driving_motor = mindstorms.Motor("B")
 
+is_active = False
+previous_color = None
 while True:
+  current_color = control_color_sensor.get_color()
+  if current_color == ACTIVATION_COLOR and current_color != previous_color:
+    is_active = not is_active
+  previous_color = current_color
+
+  if not is_active:
+    driving_motor.stop()
+    continue
+
   angle = sensory_motor.get_position()
   factor = (angle - MINIMAL_ANGLE) / (MAXIMAL_ANGLE - MINIMAL_ANGLE)
   speed = (MAXIMAL_SPEED - MINIMAL_SPEED) * factor + MINIMAL_SPEED
