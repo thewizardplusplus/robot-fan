67c7b29abb77b5b538d0ef401495b23fcd46ddb7
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-08-27 23:00:00 +0300

    Add the TogglingValue.update() method

--- robot_fan/toggling_value.py
+++ robot_fan/toggling_value.py
@@ -2,3 +2,10 @@ class TogglingValue:
     def __init__(self, reference_value):
         self.reference_value = reference_value
         self.previous_value = None
+
+    def update(self, value):
+        was_toggled = \
+            value == self.reference_value and value != self.previous_value
+        self.previous_value = value
+
+        return was_toggled
--- robot_fan/toggling_value_test.py
+++ robot_fan/toggling_value_test.py
@@ -8,3 +8,28 @@ class TestTogglingValue(unittest.TestCase):
 
         self.assertEqual(toggling_value.reference_value, 23)
         self.assertIsNone(toggling_value.previous_value)
+
+    def test_update_with_not_reference_value(self):
+        toggling_value = TogglingValue(23)
+        was_toggled = toggling_value.update(42)
+
+        self.assertEqual(toggling_value.previous_value, 42)
+        self.assertFalse(was_toggled)
+
+    def test_update_with_reference_value_and_toggling(self):
+        toggling_value = TogglingValue(23)
+        toggling_value.update(42)
+
+        was_toggled = toggling_value.update(23)
+
+        self.assertEqual(toggling_value.previous_value, 23)
+        self.assertTrue(was_toggled)
+
+    def test_update_with_reference_value_and_without_toggling(self):
+        toggling_value = TogglingValue(23)
+        toggling_value.update(23)
+
+        was_toggled = toggling_value.update(23)
+
+        self.assertEqual(toggling_value.previous_value, 23)
+        self.assertFalse(was_toggled)
