2701ea9ddad8b15dacf561f91c3e010e7a2d5766
thewizardplusplus <thewizardplusplus@yandex.ru>
2021-08-28 23:00:00 +0300

    Use the run_control_loop() function

--- robot_fan/__main__.py
+++ robot_fan/__main__.py
@@ -1,7 +1,7 @@
 import mindstorms
 
 from robot_fan.interval import Interval
-from robot_fan.toggling_value import TogglingValue
+from robot_fan.running import run_control_loop
 
 ACTIVATION_COLOR = "red"
 CONTROL_ANGLE_INTERVAL = Interval(275, 220)
@@ -11,20 +11,16 @@ ROTATE_DIRECTION = -1
 control_color_sensor = mindstorms.ColorSensor("F")
 sensory_motor = mindstorms.Motor("D")
 driving_motor = mindstorms.Motor("B")
+run_control_loop(
+    # parameters
+    activation_color=ACTIVATION_COLOR,
+    control_angle_interval=CONTROL_ANGLE_INTERVAL,
+    speed_modulus_interval=SPEED_MODULUS_INTERVAL,
+    rotate_direction=ROTATE_DIRECTION,
 
-is_active = False
-control_color = TogglingValue(ACTIVATION_COLOR)
-while True:
-  was_control_color_toggled = \
-    control_color.update(control_color_sensor.get_color())
-  if was_control_color_toggled:
-    is_active = not is_active
-
-  if not is_active:
-    driving_motor.stop()
-    continue
-
-  control_angle = sensory_motor.get_position()
-  control_factor = CONTROL_ANGLE_INTERVAL.get_proportion_by_value(control_angle)
-  speed_modulus = SPEED_MODULUS_INTERVAL.get_value_by_proportion(control_factor)
-  driving_motor.start(int(ROTATE_DIRECTION * speed_modulus))
+    # handlers
+    get_control_color=lambda: control_color_sensor.get_color(),
+    get_control_angle=lambda: sensory_motor.get_position(),
+    start_driving_motor=lambda speed: driving_motor.start(int(speed)),
+    stop_driving_motor=lambda: driving_motor.stop(),
+)
